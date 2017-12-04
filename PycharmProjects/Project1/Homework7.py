cbb926e9e47a0894130a314da90aac61bb452902
Pawel Suszek <pawel.suszek2@gmail.com>
2017-12-05 00:57:53 +0100

    trying to solve homework + link to tutorial on 'generators' topic

--- PycharmProjects/Project1/Homework7.py
+++ PycharmProjects/Project1/Homework7.py
@@ -1,1 +1,14 @@
-# zrobic cos
+
+# https://jeffknupp.com/blog/2013/04/07/improve-your-python-yield-and-generators-explained/
+
+def fibon_100(lst):
+
+    while len(lst) < 100:
+        yield lst.append(lst[-1]+lst[-2])
+    return lst
+
+lst_in = [1,2]
+gen = fibon_100(lst_in)
+
+
+print(list(gen))
