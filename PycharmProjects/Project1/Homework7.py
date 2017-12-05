42acf55b74202d12a1fad3461b6339f3f1b73afd
Pawel Suszek <pawel.suszek2@gmail.com>
2017-12-05 22:26:26 +0100

    homework 7 fibonacci sequence using generator

--- PycharmProjects/Project1/Homework7.py
+++ PycharmProjects/Project1/Homework7.py
@@ -1,14 +1,28 @@
-
-# https://jeffknupp.com/blog/2013/04/07/improve-your-python-yield-and-generators-explained/
-
-def fibon_100(lst):
-
-    while len(lst) < 100:
-        yield lst.append(lst[-1]+lst[-2])
-    return lst
-
-lst_in = [1,2]
-gen = fibon_100(lst_in)
+from datetime import datetime
 
 
-print(list(gen))
+def fibon(n):
+    lst = [0, 1]
+    yield lst[0]
+    yield lst[1]
+    for i in range(2, n + 1):
+        elem = lst[-1] + lst[-2]
+        yield elem
+        lst.append(elem)
+
+
+def main():
+    elements = 5000
+    start_time = datetime.now()
+    idx = 0
+    for it in fibon(elements):
+        print(f"fib[{idx}]={it}")
+        idx += 1
+    print(f"Calculated {elements} in {datetime.now()-start_time}")
+
+
+# Calculated 5000 in 0:00:00.090851
+
+
+if __name__ == '__main__':
+    main()
